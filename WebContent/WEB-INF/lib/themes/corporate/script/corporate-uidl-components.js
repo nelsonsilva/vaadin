296219ff70f82183db4611ff18eea53ce345e1e9
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2007-01-04 18:15:25 +0000

    empty shells
    svn changeset:223 branch:toolkit

---
+++ WebContent/WEB-INF/lib/themes/corporate/script/corporate-uidl-components.js
@@ -0,0 +1,23 @@
+
+/** Corporate theme constructor.
+ *
+ *  @param themeRoot The base URL for theme resources.
+ *  @constructor
+ *
+ */
+function CorporateTheme(themeRoot) {
+	this.themeName = "BaseTheme";
+
+	// Store the the root URL
+	this.root = themeRoot;
+}
+
+/** Register all renderers to a ajax client.
+ *
+ * @param client The ajax client instance.
+ */
+BaseTheme.prototype.registerTo = function(client) {
+
+	// Register renderer functions
+
+};
