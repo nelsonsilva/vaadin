18acf573e5659c8f08d8b1fb8768f5345594c39b
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2007-02-07 09:32:16 +0000

    Commented code, cleaned
    svn changeset:500 branch:toolkit

--- WebContent/WEB-INF/lib/themes/demo/featurebrowser/components.js
+++ WebContent/WEB-INF/lib/themes/demo/featurebrowser/components.js
@@ -27,12 +27,13 @@ registerTo : function(client) {
 	client.registerRenderer(this,"orderedlayout","featurebrowser-mainlayout",this.renderFeatureBrowserLayout);
 },
 
+/** FeatureBrowser mainlayout. This captures main level ordered layouts */
 renderFeatureBrowserLayout : function(renderer,uidl,target,layoutInfo) {
 
 	var theme = renderer.theme;
 	var pid = uidl.getAttribute("id");
 
-	// Paint layout if needed
+	// Paint layout if it is not done already
 	if (document.getElementById(pid) == null) {
 		
 		// Try to disable window scrollbars on IEs
@@ -43,24 +44,18 @@ renderFeatureBrowserLayout : function(renderer,uidl,target,layoutInfo) {
 		var paintableDiv = renderer.theme.createPaintableElement(renderer,uidl,document.body,layoutInfo);
 	 	var div = theme.createElementTo(paintableDiv,"div",null);
 		div.id="featurebrowser-mainlayout";
-		div.style.background="white";
-		div.style.backgroundImage="url("+theme.root+"featurebrowser/img/m_bg.png)";
-		div.style.backgroundRepeat="no-repeat";
 		
-		// Build layout
-		div.innerHTML = "<img src=\""+theme.root+"featurebrowser/img/header.png\"/><div id=\"featurebrowser-features\"'>features</div>"+
-		"<div id=\"featurebrowser-demo\"><table border='0' cellpadding='0' cellspacing='0' height='100%' width='100%'><tr><td align='center' valign='middle'><table><tr><td style='text-align: left;' id='featurebrowser-demo-td'> </td></tr></table></td></tr></table></div>"+
-		"<div id=\"featurebrowser-tabs\" style='background: white'>tabs</div>"+
-		"<div id=\"featurebrowser-properties\" style='width: 0px;'>properties</div>"+
-		"<img id=\"featurebrowser-properties-toggler\" src=\""+theme.root+"featurebrowser/img/show_properties.png\" style='position: absolute; top: 15px;'> </div>"+
-		"<div id=\"featurebrowser-control\"><table border='0' height='100%' width='100%'><tr><td width='50%' align='center' valign='middle' id='featurebrowser-control-left'></td><td align='center' width='50%' valign='middle' id='featurebrowser-control-right'></td></tr></table></div>" + 
-		"<div id=\"featurebrowser-divider\" style='background-image: url("+theme.root+"featurebrowser/img/tab_handle.png);'> </div>";
+		// Build layout with html
+		div.innerHTML = "<img src=\""+theme.root+"featurebrowser/img/header.png\"/>"+
+			"<div id=\"featurebrowser-features\"'>features</div>"+
+			"<div id=\"featurebrowser-demo\"><table border='0' cellpadding='0' cellspacing='0' height='100%' width='100%'><tr><td align='center' valign='middle'><table><tr><td style='text-align: left;' id='featurebrowser-demo-td'> </td></tr></table></td></tr></table></div>"+
+			"<div id=\"featurebrowser-tabs\">tabs</div>"+
+			"<div id=\"featurebrowser-properties\">properties</div>"+
+			"<img id=\"featurebrowser-properties-toggler\" src=\""+theme.root+"featurebrowser/img/show_properties.png\"> </div>"+
+			"<div id=\"featurebrowser-control\"><table border='0' height='100%' width='100%'><tr><td width='50%' align='center' valign='middle' id='featurebrowser-control-left'></td><td align='center' width='50%' valign='middle' id='featurebrowser-control-right'></td></tr></table></div>" + 
+			"<div id=\"featurebrowser-divider\" ></div>";
 		
-		// Properties hiddening
-		var propertiesDiv = document.getElementById("featurebrowser-properties");
-		propertiesDiv.targetWidth = 0;
-		
-		// Divider resize
+		// Initialize divider
 		var dividerDiv = document.getElementById("featurebrowser-divider");
 		dividerDiv.isActive = false;
 		dividerDiv.onmousedown = itmill.themes.Demo.prototype.dividerUpdate;
@@ -69,6 +64,7 @@ renderFeatureBrowserLayout : function(renderer,uidl,target,layoutInfo) {
 		window.onresize=itmill.themes.Demo.prototype.recalcFeatureBrowserLayout; 
 		theme.recalcFeatureBrowserLayout();
 	}
+	
 	
 	// Render UIDL to layout
 	
@@ -95,7 +91,7 @@ renderFeatureBrowserLayout : function(renderer,uidl,target,layoutInfo) {
 	theme.removeAllChildNodes(target);
 	renderer.client.renderUIDL(theme.elementByIndex(theme.elementByIndex(uidl.childNodes,1).childNodes,1),target);
 
-	// Props
+	// Properties and property button
 	target = document.getElementById("featurebrowser-properties")
 	theme.removeAllChildNodes(target);
 	var buttonContainer = theme.createElementTo(target,"div",null);
@@ -126,10 +122,12 @@ dividerUpdate : function() {
 	return false;
 },
 
+/** Property hiding button renderer */
 renderCheckBox : function(renderer,uidl,target,layoutInfo) {
 
 		arguments.callee.$.renderCheckBox.call(this,renderer,uidl,target,layoutInfo);
 		
+		// If this is the property hiding button
 		if (target.propPanelDummy || target.parentNode.propPanelDummy) {
 			var theme = renderer.theme;
 			var propertiesDiv = document.getElementById("featurebrowser-properties");
@@ -146,6 +144,7 @@ renderCheckBox : function(renderer,uidl,target,layoutInfo) {
 		}
 },
 
+/** Property panel hiding button click event handler */
 propertiesButtonClick : function() {
 	var propertiesDiv = document.getElementById("featurebrowser-properties");
 	propertiesDiv.targetWidth = propertiesDiv.buttonState == "false" ? propertiesDiv.maxWidth : 0;
@@ -153,126 +152,128 @@ propertiesButtonClick : function() {
 	client.changeVariable(propertiesDiv.buttonId,propertiesDiv.buttonState == "false" ? "true" : "false",true);
 },
 
+/** Recalculate feature browser main layout dimensions for all components */
 recalcFeatureBrowserLayout : function() {
 
-		var animationNeeded = false;
+	// Then animations are needed, this will be reset to true
+	var animationNeeded = false;
 
-		var mainDiv = document.getElementById("featurebrowser-mainlayout"); 
-		var featuresDiv = document.getElementById("featurebrowser-features");
-		var demoDiv = document.getElementById("featurebrowser-demo");
-		var tabsDiv = document.getElementById("featurebrowser-tabs");
-		var propertiesDiv = document.getElementById("featurebrowser-properties");
-		var controlDiv = document.getElementById("featurebrowser-control");
-		var dividerDiv = document.getElementById("featurebrowser-divider");
+	// References to all elements
+	var mainDiv = document.getElementById("featurebrowser-mainlayout"); 
+	var featuresDiv = document.getElementById("featurebrowser-features");
+	var demoDiv = document.getElementById("featurebrowser-demo");
+	var tabsDiv = document.getElementById("featurebrowser-tabs");
+	var propertiesDiv = document.getElementById("featurebrowser-properties");
+	var controlDiv = document.getElementById("featurebrowser-control");
+	var dividerDiv = document.getElementById("featurebrowser-divider");
 
-		// Logobar
-		var logoBarHeight = 62; // TODO ADJUST THIS TO LOGO HEIGHT
+	// Logobar height
+	var logoBarHeight = 62;
 
-		// Recalc main div dimensions
-		mainDiv.style.position="absolute";
-		mainDiv.style.overflow="hidden";
-		mainDiv.style.top="0";
-		mainDiv.style.left="0";
-		mainDiv.style.width="100%";
-		mainDiv.style.height="100%";
-		var width = mainDiv.offsetWidth;
-		var height = mainDiv.offsetHeight;
-		if (height < 400 || width < 600) {
-			height = 400;
-			width = 600;
-			if (document.body.offsetWidth > width) width = document.body.offsetWidth;
-			// TODO This does work in IE6, but not in IE7 ?!
-			if (document.body.offsetHeight > height) height = document.body.offsetHeight;
-			mainDiv.style.width="" + width + "px";
-			mainDiv.style.height="" + height + "px";
-		}
+	// Get whole window contents size
+	var width = mainDiv.offsetWidth;
+	var height = mainDiv.offsetHeight;
+	
+	// Set minimum size for window
+	if (height < 400 || width < 600) {
+		height = 400;
+		width = 600;
 		
-		// Recalc features div dimensions
-		var featuresWidth = 200;
-		var controlHeight = 50;
-		featuresDiv.style.position="absolute";
-		featuresDiv.style.overflow="auto";
-		featuresDiv.style.top="" + logoBarHeight + "px";
-		featuresDiv.style.left="0";
-		featuresDiv.style.width="" + featuresWidth + "px";
-		featuresDiv.style.height="" + (height-controlHeight-logoBarHeight) + "px";
-		
-		// Recalc properties div dimensions
-		var propWidth = Math.floor((propertiesDiv.offsetWidth + propertiesDiv.targetWidth)/2);
-		if (Math.abs(propWidth - propertiesDiv.targetWidth) <= 1) propWidth = propertiesDiv.targetWidth;
-		if (propWidth >width-featuresWidth) propWidth = width-featuresWidth; 
-		if (propWidth < 0) propWidth = 0; 
-		if ((propWidth+1) != (propertiesDiv.targetWidth+1)) animationNeeded=true;
-		var centerWidth = width - propWidth - featuresWidth - 40;
-		propertiesDiv.style.position="absolute";
-		propertiesDiv.style.overflow="hidden";
-		propertiesDiv.style.top="" + 15 + "px";
-		propertiesDiv.style.left="" + (centerWidth + featuresWidth + 40) + "px";
-		propertiesDiv.style.width=propWidth + "px";
-		propertiesDiv.style.height="" + (height - 15) + "px";	
-		var buttonDiv = document.getElementById("featurebrowser-properties-toggler");
-		buttonDiv.style.left = "" + (centerWidth + featuresWidth - 20 + 40) + "px"
-		if (propWidth == 0) buttonDiv.src = buttonDiv.showPng;
-		if (propWidth == propertiesDiv.maxWidth) buttonDiv.src = buttonDiv.hidePng;
-		itmill.themes.Demo.prototype.updatePropertiesContentHeight();
-		
-		// Recalc divider div dimensions
-		if (typeof dividerDiv.demoHeight == 'undefined') dividerDiv.demoHeight = Math.floor(height/2);
-		if (dividerDiv.isActive) {
-			dividerDiv.demoHeight = dividerDiv.mouseY-10 - logoBarHeight;
-			if (dividerDiv.demoHeight < 0) dividerDiv.demoHeight = 0;
-		} 
-		var dividerHeight = 17;
-		dividerDiv.style.position="absolute";
-		dividerDiv.style.overflow="hidden";
-		dividerDiv.style.top="" + (dividerDiv.demoHeight + logoBarHeight)+ "px";
-		dividerDiv.style.left="" + (15+featuresWidth+Math.round((centerWidth-137)/2)) +"px";
-		dividerDiv.style.width="" + 137 + "px";
-		dividerDiv.style.height="" + dividerHeight + "px";		
-		
-		// Recalc tabs div dimensions
-		tabsDiv.style.position="absolute";
-		tabsDiv.style.overflow="hidden";
-		tabsDiv.style.top="" + (dividerDiv.demoHeight + dividerHeight + logoBarHeight) + "px";
-		tabsDiv.style.left="" + (featuresWidth + 15) + "px";
-		tabsDiv.style.width="" + centerWidth + "px";
-		tabsDiv.style.height="" + (height - dividerDiv.demoHeight - dividerHeight - logoBarHeight) + "px";	
-		itmill.themes.Demo.prototype.updateTabsContentHeight();	
-		
-		// Recalc demo div dimensions
-		demoDiv.style.position="absolute";
-		demoDiv.style.overflow="auto";
-		demoDiv.style.top="" + logoBarHeight + "px";
-		demoDiv.style.left="" + (featuresWidth + 15) + "px";
-		demoDiv.style.width="" + centerWidth + "px";
-		demoDiv.style.height="" + dividerDiv.demoHeight + "px";	
-		
-		// Recalc control div dimensions
-		// TODO, make floating
-		controlDiv.style.position="absolute";
-		controlDiv.style.overflow="hidden";
-		controlDiv.style.top="" + (height - controlHeight) + "px";
-		controlDiv.style.left="0";
-		controlDiv.style.width="" + featuresWidth + "px";
-		controlDiv.style.height="" + controlHeight + "px";	
-		
-		if (animationNeeded) {
-			setTimeout("itmill.themes.Demo.prototype.recalcFeatureBrowserLayout()",30);
-		}
+		// Fix IE bugs
+		if (document.body.offsetWidth > width) width = document.body.offsetWidth;
+		if (document.body.offsetHeight > height) height = document.body.offsetHeight;
+
+		mainDiv.style.width="" + width + "px";
+		mainDiv.style.height="" + height + "px";
+	}
+	
+	// Recalc features div dimensions
+	var featuresWidth = 200;
+	var controlHeight = 50;
+	featuresDiv.style.top="" + logoBarHeight + "px";
+	featuresDiv.style.width="" + featuresWidth + "px";
+	featuresDiv.style.height="" + (height-controlHeight-logoBarHeight) + "px";
+	
+	// Recalc properties div dimensions
+	if (typeof propertiesDiv.targetWidth == 'undefined') propertiesDiv.targetWidth = 0;
+	var propWidth = Math.floor((propertiesDiv.offsetWidth + propertiesDiv.targetWidth)/2);
+	if (Math.abs(propWidth - propertiesDiv.targetWidth) <= 1) propWidth = propertiesDiv.targetWidth;
+	if (propWidth >width-featuresWidth) propWidth = width-featuresWidth; 
+	if (propWidth < 0) propWidth = 0; 
+	if ((propWidth+1) != (propertiesDiv.targetWidth+1)) animationNeeded=true;
+	var centerWidth = width - propWidth - featuresWidth - 40;
+	propertiesDiv.style.position="absolute";
+	propertiesDiv.style.overflow="hidden";
+	propertiesDiv.style.top="" + 15 + "px";
+	propertiesDiv.style.left="" + (centerWidth + featuresWidth + 40) + "px";
+	propertiesDiv.style.width=propWidth + "px";
+	propertiesDiv.style.height="" + (height - 15) + "px";	
+	var buttonDiv = document.getElementById("featurebrowser-properties-toggler");
+	buttonDiv.style.left = "" + (centerWidth + featuresWidth - 20 + 40) + "px"
+	if (propWidth == 0) buttonDiv.src = buttonDiv.showPng;
+	if (propWidth == propertiesDiv.maxWidth) buttonDiv.src = buttonDiv.hidePng;
+	itmill.themes.Demo.prototype.updatePropertiesContentHeight();
+	
+	// Recalc divider div dimensions
+	if (typeof dividerDiv.demoHeight == 'undefined') dividerDiv.demoHeight = Math.floor(height/2);
+	if (dividerDiv.isActive) {
+		dividerDiv.demoHeight = dividerDiv.mouseY-10 - logoBarHeight;
+		if (dividerDiv.demoHeight < 0) dividerDiv.demoHeight = 0;
+	} 
+	var dividerHeight = 17;
+	dividerDiv.style.position="absolute";
+	dividerDiv.style.overflow="hidden";
+	dividerDiv.style.top="" + (dividerDiv.demoHeight + logoBarHeight)+ "px";
+	dividerDiv.style.left="" + (15+featuresWidth+Math.round((centerWidth-137)/2)) +"px";
+	dividerDiv.style.width="" + 137 + "px";
+	dividerDiv.style.height="" + dividerHeight + "px";		
+	
+	// Recalc tabs div dimensions
+	tabsDiv.style.position="absolute";
+	tabsDiv.style.overflow="hidden";
+	tabsDiv.style.top="" + (dividerDiv.demoHeight + dividerHeight + logoBarHeight) + "px";
+	tabsDiv.style.left="" + (featuresWidth + 15) + "px";
+	tabsDiv.style.width="" + centerWidth + "px";
+	tabsDiv.style.height="" + (height - dividerDiv.demoHeight - dividerHeight - logoBarHeight) + "px";	
+	itmill.themes.Demo.prototype.updateTabsContentHeight();	
+	
+	// Recalc demo div dimensions
+	demoDiv.style.position="absolute";
+	demoDiv.style.overflow="auto";
+	demoDiv.style.top="" + logoBarHeight + "px";
+	demoDiv.style.left="" + (featuresWidth + 15) + "px";
+	demoDiv.style.width="" + centerWidth + "px";
+	demoDiv.style.height="" + dividerDiv.demoHeight + "px";	
+	
+	// Recalc control div dimensions
+	// TODO, make floating
+	controlDiv.style.position="absolute";
+	controlDiv.style.overflow="hidden";
+	controlDiv.style.top="" + (height - controlHeight) + "px";
+	controlDiv.style.left="0";
+	controlDiv.style.width="" + featuresWidth + "px";
+	controlDiv.style.height="" + controlHeight + "px";	
+	
+	if (animationNeeded) {
+		setTimeout("itmill.themes.Demo.prototype.recalcFeatureBrowserLayout()",30);
+	}
 },
 
+/** Adjust tabsheet rendering on main layout */
 renderTabSheet : function(renderer,uidl,target,layoutInfo) {
 	arguments.callee.$.renderTabSheet.call(this,renderer,uidl,target,layoutInfo);
 	if (target.id == "featurebrowser-tabs" || target.parentNode.id ==  "featurebrowser-tabs") 
 		itmill.themes.Demo.prototype.updateTabsContentHeight();
 },
 
+/** Adjust property panel rendering on main layout */
 renderPanel : function(renderer,uidl,target,layoutInfo) {
 	arguments.callee.$.renderPanel.call(this,renderer,uidl,target,layoutInfo);
 	if (target.id == "featurebrowser-properties" || target.parentNode.id ==  "featurebrowser-properties") 
 		itmill.themes.Demo.prototype.updatePropertiesContentHeight();
 },
 
+/** Recalculate tabs content width */
 updateTabsContentHeight : function() {
 	try {
 		var tabsDiv = document.getElementById("featurebrowser-tabs");
@@ -285,6 +286,7 @@ updateTabsContentHeight : function() {
 	} catch (e) {}
 },
 
+/** Recalculate property panel content width */
 updatePropertiesContentHeight : function() {
 	try {
 		var propsDiv = document.getElementById("featurebrowser-properties");
@@ -297,6 +299,7 @@ updatePropertiesContentHeight : function() {
 	} catch (e) {}
 },
 
+/** Helper method. Get element from node array by index */
 elementByIndex : function(nodeArray, index) {
 	var i=0;
 	while (index>=0) {
