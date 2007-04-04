01bd05335b9ef54ac4bee174388f3e37fe02df28
Jani Laakso <jani.laakso@itmill.com>
2007-04-04 08:50:03 +0000

    Fixed notes displayed when starting demos.
    svn changeset:1135 branch:trunk

--- build/demo/start.bat
+++ build/demo/start.bat
@@ -1,4 +1,4 @@
-@echo Starting IT Mill Toolkit DEMO
-@echo You can access the demo in http://127.0.0.1:8888/
-@echo Use ctrl-c to stop demo service
+@echo Starting IT Mill Toolkit demo
+@echo Please open your web browser and access the demo in http://127.0.0.1:8888/
+@echo Use ctrl-c here to stop demo service
 @java -DDEBUG_VERBOSE=-1 -jar start.jar
