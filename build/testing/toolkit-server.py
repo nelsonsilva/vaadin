4db3055735c1ddcbd8c109d6bf661261f695e1a5
Mikael Grankvist <mikael.grankvist@itmill.com>
2009-09-14 12:44:41 +0000

    Component test cases
    svn changeset:8760 branch:6.1

--- build/testing/toolkit-server.py
+++ build/testing/toolkit-server.py
@@ -5,7 +5,7 @@ import sys,os,datetime,subprocess,time
 ################################################################################
 # Configuration
 ################################################################################
-LOCKFILE = "/var/lock/itmill-toolkit-server"
+LOCKFILE = "/var/lock/vaadin-server"
 
 ################################################################################
 # Tools
@@ -14,22 +14,37 @@ def execute(cmd):
 	# print cmd
 	return os.system(cmd)
 
-# Unpacks the Toolkit installation package to a target location.
+# Unpacks the Vaadin installation package to a target location.
 # The package file name must have absolute path.
-def unpackToolkit(packagefile, targetdir):
-	cmd = "tar zxf %{PF} -C ${TD}" % {"PF": packagefile, "TD": targetdir}
+def unpackVaadin(packagefile, targetdir):
+	cmd = "tar zxf %{PF}s -C ${TD}s" % {"PF": packagefile, "TD": targetdir}
 	if execute(cmd):
-		print "Unpacking Toolkit installation package %{PF} to ${TD} failed." % {"PF": packagefile, "TD": targetdir}
+		print "Unpacking Vaadin installation package %{PF} to ${TD} failed." % {"PF": packagefile, "TD": targetdir}
 		sys.exit(1)
 
-def startToolkit(packagename, testarea):
-	print "Starting Toolkit server in %s/%s" % (testarea, packagename)
+################################################################################
+# startVaadin()
+# Starts the Vaadin server process.
+################################################################################
+def startVaadin(packagename, testarea):
+	print "Starting Vaadin server in %s/%s" % (testarea, packagename)
 	os.chdir ("%s/%s" % (testarea, packagename));
+
+	libdir = testarea + "/" + packagename + "/WebContent/WEB-INF/lib"
+
+	classpath = "WebContent/demo/lib/jetty/jetty-6.1.7.jar:"+\
+				"WebContent/demo/lib/jetty/jetty-util-6.1.7.jar:"+\
+				"WebContent/demo/lib/jetty/servlet-api-2.5-6.1.7.jar:"+\
+				"WebContent/WEB-INF/classes:WebContent/WEB-INF/src"
+	classpath += ":%(libdir)s/testbench/*:%(libdir)s/testbench/lib/*" % {"libdir": libdir}
+
+	javacmd = "java -cp %(classpath)s com.vaadin.launcher.DemoLauncher --nogui=1" % {"classpath": classpath}
+	print javacmd
 
     # All the stdin, stdout, and stderr must be redirected
     # or otherwise Ant will hang when this start script returns.
-	if execute("VAADIN_PARAMETERS='--nogui=1' nohup sh start.sh </dev/null >WebContent/nohup.txt 2>&1 &"):
-		print "Launching Toolkit server failed."
+	if execute(javacmd + " </dev/null >WebContent/nohup.txt 2>&1 &"):
+		print "Launching Vaadin server failed."
 
 	# Wait a little to let it start.
 	time.sleep(5)
@@ -40,14 +55,14 @@ def stopProcess(pin):
 		pin.close()
 		pid = pid.rstrip('\n')
 		if len(pid)>0:
-			print "Killing existing Toolkit demo, PID [" + pid + "]"
+			print "Killing existing Vaadin demo, PID [" + pid + "]"
 			execute("kill -9 " + pid)
 			time.sleep(5);
-			print "Killing existing Toolkit demo, PID [" + pid + "]"
+			print "Killing existing Vaadin demo, PID [" + pid + "]"
 			execute("kill -9 " + pid)
 			time.sleep(2);
 	
-def stopToolkit():
+def stopVaadin():
 	pin = os.popen("ps -do pid,args | grep DemoLauncher | grep -v grep | grep java | sed -e 's/^ \\+//' | cut -d ' ' -f 1", "r")
 	stopProcess(pin)
 	pin = os.popen("ps -do pid,args | grep DevelopmentServerLauncher | grep -v grep | grep java | sed -e 's/^ \\+//' | cut -d ' ' -f 1", "r")
@@ -57,32 +72,62 @@ def stopToolkit():
 # Commands
 ################################################################################
 
-def commandStart(packagename, packagefile, testarea):
+def commandStart(packagename, packagefile, outputdir, testarea):
 	# Remove old build
+	print "Cleaning test area '%s'..." % (testarea)
 	if len(testarea) < 3:
 		print "The test area directory may not be too short: %s" % (testarea)
 		sys.exit(1)
 	execute("rm -rf %s/vaadin-*" % (testarea))
 		
-	print "Creating test area '%s' if it does not already exist." % (testarea)
+	print "Creating test area '%s' if it does not already exist..." % (testarea)
 	if execute ("mkdir -p %s" % testarea):
 		print "Creation of test area '%s' failed." % (testarea)
 		sys.exit(1)
 		
-	print "Extracting Toolkit package '%s' to test area '%s'..." % (packagefile, testarea)
+	print "Extracting Vaadin package '%s' to test area '%s'..." % (packagefile, testarea)
 	if execute ("tar zxf %s -C %s" % (packagefile, testarea)):
-		print "Extracting Toolkit package failed."
+		print "Extracting Vaadin package failed."
+		sys.exit(1)
+
+	resultpath       = outputdir[:outputdir.rfind("/")]
+	installationpath = testarea + "/" + packagename
+
+	# Copy extra class files from the output directory to the test area. (#3325)
+	classsrc  = resultpath + "/classes/com/vaadin/tests"
+	classtrg  = installationpath + "/WebContent/WEB-INF/classes/com/vaadin/tests"
+	print "Copying all class files from %s to %s..." % (classsrc, classtrg)
+	if execute ("cp -r %s %s" % (classsrc, classtrg)):
+		print "Copying class files failed."
+		sys.exit(1)
+
+	# TODO (#3325): Copy test themes, etc.
+	webcontent = "WebContent"
+	themepath  = webcontent + "/VAADIN/themes"
+	testthemes = themepath + "/tests-*"
+	themetrg   = installationpath + "/WebContent/VAADIN/themes/"
+	print "Copying themes from %s to %s..." % (testthemes, themetrg)
+	if execute ("cp -r %s %s" % (testthemes, themetrg)):
+		print "Copying theme files failed."
+		sys.exit(1)
+
+	# Copy testbench libraries.
+	testbenchdir = "build/lib/testbench"
+	libdir       = installationpath + "/WebContent/WEB-INF/lib"
+	print "Copying testbench libraries from '%s' to '%s'" % (testbenchdir, libdir)
+	if execute ("cp -r  %s %s/" % (testbenchdir, libdir)):
+		print "Copying testbench libraries to test installation failed."
 		sys.exit(1)
 	
-	# Start new Toolkit demo service
-	startToolkit(packagename, testarea)
+	# Start new Vaadin demo service
+	startVaadin(packagename, testarea)
 	
 	# Wait for the service to start
 	print "Waiting a bit for Servlet Container to start up."
 	time.sleep(5);
 
 def commandStop():
-	stopToolkit()
+	stopVaadin()
 
 	# allow next instance to run
 	execute("rm -f %s" % (LOCKFILE))
@@ -99,7 +144,8 @@ commandStop()
 if command == "start" or command == "restart":
 	packagename = sys.argv[2]
 	packagefile = sys.argv[3]
-	testarea    = sys.argv[4]
-	commandStart(packagename, packagefile, testarea)
+	outputdir   = sys.argv[4]
+	testarea    = sys.argv[5]
+	commandStart(packagename, packagefile, outputdir, testarea)
 
 print "Done."
